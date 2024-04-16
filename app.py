489ed0369d01e67168267607c167d73f73bdf44d
shrey74 <shreymishra747890@gmail.com>
2024-04-17 00:11:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from ayurveda import send_message_and_get_response
+from pydantic import BaseModel
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Item(BaseModel):
+    message: str
+
+
+@app.get("/api/test")
+async def test():
+    return "Hello World"
+
+
+@app.post("/api/chat")
+async def chat(data: Item):
+    return send_message_and_get_response(data.message)
---
+++ keys.py
@@ -0,0 +1,10 @@
+# assistant_id = "asst_4e4nyTmSBdg1SlZlElffFfAu"
+# thread_id = "thread_bfk7hbLSwhsgZVFdsdHu5dC0"
+
+
+# assistant_id = "asst_H7wZyaQUPmVLbLC4jH9VKQg5"
+# thread_id = "thread_9xoREsz84IzMUwvRUYqswvFZ"
+
+
+assistant_id = "asst_2WPKsBzU5W0dBls6IzMMdfbh"
+thread_id = "thread_G96N5IdKLOE36MhTwvrVe6U5"
